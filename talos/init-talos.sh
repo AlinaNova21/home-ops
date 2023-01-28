5a0a1978a249416c49b52aef1c0ef5e9d9e7032b
Alina Shumann <ashumann13@gmail.com>
2023-01-27 23:02:16 -0600

    metallb ips

--- talos/init-talos.sh
+++ talos/init-talos.sh
@@ -11,3 +11,9 @@ for YML in ./clusterconfig/*.yaml; do
         --file $YML \
         $@
 done
+while ! kubectl get nodes 2>/dev/null 1>/dev/null; do echo Waiting on api; sleep 10; done
+kubectl kustomize --enable-helm ../kubernetes/bootstrap | kubectl apply -f -
+cat ~/.config/sops/age/keys.txt | kubectl -n flux-system create secret generic sops-age --from-file=age.agekey=/dev/stdin
+sops --decrypt ../kubernetes/flux/vars/cluster-secrets.sops.yaml | kubectl apply -f -
+kubectl apply -f ../kubernetes/flux/vars/cluster-settings.yaml
+kubectl apply --kustomize ../kubernetes/flux/config
