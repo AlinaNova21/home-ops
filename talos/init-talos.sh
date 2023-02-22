434212bafeb348f3ad8fa8ffa621150223565507
Alina Shumann <ashumann13@gmail.com>
2023-02-22 14:52:51 -0600

    Fix talos init script

--- talos/init-talos.sh
+++ talos/init-talos.sh
@@ -13,7 +13,10 @@ for YML in ./clusterconfig/*.yaml; do
 done
 while ! kubectl get nodes 2>/dev/null 1>/dev/null; do echo Waiting on api; sleep 10; done
 kubectl kustomize --enable-helm ./ | kubectl apply -f -
+kubectl apply -k ../kubernetes/bootstrap
 cat ~/.config/sops/age/keys.txt | kubectl -n flux-system create secret generic sops-age --from-file=age.agekey=/dev/stdin
 sops --decrypt ../kubernetes/flux/vars/cluster-secrets.sops.yaml | kubectl apply -f -
 kubectl apply -f ../kubernetes/flux/vars/cluster-settings.yaml
 kubectl apply --kustomize ../kubernetes/flux/config
+
+# TODO: implement this as Taskfile
